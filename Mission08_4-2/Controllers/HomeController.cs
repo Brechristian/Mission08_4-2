bc9f67a48648315fbbddc43172e4bffb8f9b5055
Christian <cbreshe1@byu.edu>
2024-02-26 16:03:32 -0700

    setting up basic formatting

--- Mission08_4-2/Controllers/HomeController.cs
+++ Mission08_4-2/Controllers/HomeController.cs
@@ -6,27 +6,13 @@ namespace Mission08_4_2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private IToDoListRepository _repo;
+        public HomeController(IToDoListRepository temp)
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            _repo = temp;
         }
     }
+
 }
+
+        
---
+++ Mission08_4-2/Models/IToDoListRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+namespace Mission08_4_2.Models
+{
+
+    // need to include required columns
+    //Task, Quadrant
+    // category and completed are not technically "Required", however they are dropdowns so they will always be included
+    // .
+    public class IToDoListRepository
+    {
+        List<InserttableNameHere> insertName { get; }
+
+        public void AddtableName(TableName name);
+    }
+}
