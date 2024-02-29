b0b66f53d158a03772b1a8e8c371b696c21ed799
Supermarauder <119464479+Supermarauder@users.noreply.github.com>
2024-02-29 15:54:55 -0700

    set up the repositories

---
+++ Mission08_4-2/Models/EFTasksRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mission08_4_2.Models
+{
+    public class EFTasksRepository : ITasksRepository
+    {
+        private TasksContext _context;
+
+        public EFTasksRepository(TasksContext temp) 
+        {
+            _context = temp;
+        }
+
+        public List<Tasks> Tasks => _context.Tasks.ToList();
+        public List<Quadrant> Quadrants => _context.Quadrants.ToList();
+        public List<Category> Categories => _context.Categories.ToList();
+
+        public void AddTask(Task task) 
+        {
+            _context.Add(task);
+            _context.SaveChanges();
+        }
+    }
+}
