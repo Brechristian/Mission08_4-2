92581a16814186b7400bb7f2b0952be1cb80b02f
Supermarauder <119464479+Supermarauder@users.noreply.github.com>
2024-03-01 10:54:29 -0700

    added edit/update and delete methods

--- Mission08_4-2/Models/ITasksRepository.cs
+++ Mission08_4-2/Models/ITasksRepository.cs
@@ -5,7 +5,7 @@ namespace Mission08_4_2.Models
     public interface ITasksRepository
     {
         List<Tasks> Tasks { get; }
-       public void AddTask (Task task);
+       public void AddTask (Tasks task);
         List<Category> Categories { get;}
         List<Quadrant> Quadrants { get; }
 
